3abf1d1a588975630f89c46433eb1d8b92feab15
Pallavi Shah <emailpallavishah@gmail.com>
2019-11-08 16:49:23 -0600

    files updated

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // TODO: add code here
+
+window.addEventListener("load",function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+      console.log(response);
+      response.json().then(function(json){
+          console.log(json);
+          let var_container=document.getElementById("container");
+          for(index=0;index<json.length;index++){
+            var_activeClass="";
+            if(json[index].active===true){
+              var_activeClass="active";
+            }
+          var_container.innerHTML+=`<div class="astronaut">
+            <div> <b>${json[index].firstName} ${json[index].lastName}</b>
+            <ul> 
+              <li>hoursInSpace: ${json[index].hoursInSpace}</li>
+              <li class="${var_activeClass}"> Active: ${json[index].active}</li>
+              <li>Skills:${json[index].skills}</li> 
+            </ul>
+          </div>  <img src="${json[index].picture}" class="avatar"/> </div>`;
+        }
+                document.getElementById("totalastronauts").innerHTML+=json.length;
+      });
+    });
+});// TODO: add code here
